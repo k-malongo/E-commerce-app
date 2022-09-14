590669581923ca51f264a282c970340b60705871
Kelvin <vokemalosh32@gmail.com>
2022-09-14 15:21:37 +0300

    fixing bugs

--- src/components/Home/Cards.js
+++ src/components/Home/Cards.js
@@ -36,7 +36,7 @@ const Cards = ({ card }) => {
 
            
             <Grid item xs={12} sm={6} lg={3} md={3} style={{ marginBottom: "14" }} key={data.id}>
-              <Card sx={{ maxWidth: 350, marginLeft: "25px", marginRight: "1px", p:"2" }} className={classes.card}>
+              <Card sx={{ maxWidth: 350, marginLeft: "50px", marginRight: "1px", p:"2" }} className={classes.card}>
                 <div
                   style={{ margin: "10px" }}
                 >
